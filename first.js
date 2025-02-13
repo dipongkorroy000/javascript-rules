c665833fb0d1fb3656b5fddeaeb2535a35e80134
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-13 16:34:25 +0600

    add file:

--- first.js
+++ first.js
@@ -28,7 +28,7 @@
 //     console.log('you can take');
 // }
 
-// type-6--array->
+// type-6--array--->
 // let numbers = [55,23,24,16,59,75,52,34];
 // console.log(Array.isArray(numbers)); // test the numbers attribute type---
 // console.log(numbers.length);
@@ -41,7 +41,7 @@
 // console.log(numbers.includes(52)); //finding number---
 // console.log(numbers.indexOf(52)); // finding index number to 52---
 
-// type-7-loop--
+// type-7-loop--->
 // let students = ['rahim','karim','jadu','madu','sajjat'];
 // for(let a of students){
 //     console.log(a);
@@ -72,7 +72,7 @@
 // flowerName[2] = 'puspa'; // changed the value, array is mutable---
 // console.log(flowerName);
 
-//type-9-lowercase-uppercase-&-trim->
+//type-9-lowercase-uppercase-&-trim--->
 // let schoolName = 'North IDEAL Academy';
 // console.log(schoolName);
 // console.log(schoolName.toLowerCase());
@@ -86,4 +86,146 @@
 // let liquid = 'water '
 // console.log(drink.trim() == liquid.trim()); // trim just side space out ---
 
-//type-10-
+// type-10-string part need-slice(),--->
+// let fullName = 'dipongkorroy';
+// console.log(fullName.slice(0,3)); // result- dip---
+
+// type-11-string-&-array-- split()---->
+// let schoolName = 'North IDEAL Academy';
+// console.log(schoolName.split(' ')); //result- [ 'North', 'IDEAL', 'Academy' ]---
+// again example---
+// let students = 'rahim,karim,jadu,madu,sajjat';
+// console.log(students.split(',')); // result- [ 'rahim', 'karim', 'jadu', 'madu', 'sajjat' ]---
+
+// type-12-array-&-string--join()---->
+// let flowerName = ['golap','joba','sondha maloti'];
+// console.log(flowerName.join()); // result--string out---
+
+// let students = ['rahim','karim','jadu','madu','sajjat'];
+// console.log(students.join('|')); 
+// console.log(students.join('-'));
+
+// type-13--string join- concat()--->
+// let first = 'hero';
+// let last = 'alom';
+// console.log(first + ' ' + last);
+// // alternative --
+// console.log(first.concat(' ').concat(last));
+
+// type-14-string reverse--->
+// let demoName = 'I am learning Web Dev.';
+
+// let s = '';
+// for(let d of demoName){
+//     s = d + s;
+// }
+// console.log(s);
+
+//alternative---
+// let s = '';
+// for(let d = 0;d < demoName.length;d++){
+//     s = demoName[d] + s;
+// }
+// console.log(s);
+
+//alternative---
+// let s = demoName.split('').reverse().join('');
+// console.log(s);
+
+// type-15--objects-1--->
+// let person = {
+//     fullName: 'mahabub alom',
+//     age: 25,
+//     married: false,
+//     'fav places': ['ramsagor','kuakata','bandorban','jaflaong']
+// }
+
+// all key/property showing---
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+
+//finding key/property value---
+// console.log(typeof person);
+// console.log(person.fullName); //or--
+// console.log(person['fullName']);
+// console.log(person["fav places"]); // must be-for array--
+
+//update/change value---
+// person.fullName = 'jankar mahabub vai'; // or--
+// person['age'] = 28;
+// person['fav places'] = ['suksagor','coxbazar'];
+// console.log(person);
+
+//finding name-variable system---
+// let d = 'fullName';
+// console.log(person[d]);
+
+// update name-variable system---
+// let s = 'fullName';
+// person[s] = 'mahabub';
+// console.log(person);
+
+// type-16-object-2--->
+// let college = {
+//     fullName: 'ADC',
+//     class: ['11','12'],
+//     events: ['science fair','bijoy dibos','21 feb'],
+//     students: {
+//         class11: 512,
+//         class12: {
+//             gpa: 60,
+//             merit: 'top'
+//         }
+//     }
+// }
+
+//finding key/property value---
+// console.log(college.students.class11);
+// console.log(college.students.class12.gpa);
+// console.log(college.events[1]);
+
+//update/change value---
+// college.students.class12.gpa = 58;
+// college.events[1] = '16 december';
+
+// // key/property delete---
+// delete college.fullName;
+
+// console.log(college);
+
+// type-17-object-3--->
+// let mobile = {
+//     brand: 'samsung',
+//     price: 25000,
+//     color: 'white',
+//     camera: '12mp'
+// }
+
+// all key & value showing step by step---
+// for(let s in mobile){
+//     console.log(s);
+//     console.log(mobile[s]);
+// } 
+
+// all key & value showing one by one---
+// for(let s in mobile){
+//     console.log(s,mobile[s]);
+// }
+// //or object literal--
+// for(let s in mobile){
+//     console.log(s,':',mobile[s]);
+// }
+
+// key showing array---
+// console.log(Object.keys(mobile));
+
+// alternative key showing loop---
+// for(let d in mobile){
+//     console.log(d);
+// }
+
+// type-18-object create method--->
+//1. let pen = {};
+//2. let pen = new Object();
+//3. let pen = Object.create();
+
